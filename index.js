d25fe46cbeadbafbc1bc1ceb2a777ec20a81c707
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-11-24 13:31:14 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: process.env.BOT_TOKEN });
+
+manager.spawn();
+manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
